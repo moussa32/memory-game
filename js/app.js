f16280804896909a1df9e2edc15ba40360637a84
moussa32 <moussaebrahem32@gmail.com>
2021-01-19 23:49:35 +0200

    matching function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 /*
  * Create a list that holds all of your cards
- */
+*/
 
+let cards = [...document.querySelectorAll('.card')];
 
 /*
  * Display the cards on the page
@@ -9,6 +10,7 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -26,6 +28,87 @@ function shuffle(array) {
 }
 
 
+let initialGame = () => {
+    //Select old ul[calss='deck']
+    const deck = document.querySelector('.deck');
+
+    //Remove hard coded html cards
+    deck.innerHTML = '';
+
+    //Make new shuffled cards
+    shuffledCards = shuffle(cards);
+    
+    shuffledCards.map((shuffledCard) => {
+
+        //Append new shuffled cards into html
+        deck.appendChild(shuffledCard);
+
+        shuffledCard.addEventListener('click', flipCard);
+    });
+}
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    classState('toggle', this, 'open', 'show');
+
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+
+        return;
+    }
+
+    hasFlippedCard = false;
+    secondCard = this;
+    matching();
+}
+
+//Matching the cards
+function matching(){
+
+    let isCardsMatching = firstCard.innerHTML === secondCard.innerHTML;
+
+    isCardsMatching ? disableCards() : unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    classState('add', firstCard, 'match');
+    classState('add', secondCard, 'match');
+}
+
+function unflipCards(){
+    setTimeout(() => {
+        classState('remove', firstCard, 'show', 'open');
+        classState('remove', secondCard, 'show', 'open');
+    }, 800);
+}
+
+
+
+
+
+
+
+function classState(state, element, firstClass = null, secondClass = null){
+    if(state === 'add'){
+        element.classList.add(firstClass);
+        element.classList.add(secondClass);
+    }else if(state === 'toggle'){
+        element.classList.toggle(firstClass);
+        element.classList.toggle(secondClass);
+    }else if (state === 'remove'){
+        element.classList.remove(firstClass);
+        element.classList.remove(secondClass);
+    }
+}
+
+initialGame();
+
+
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -36,3 +119,4 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
